d9d992ce0bf3ded171c841d118cf7a2afae3a2f6
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-02-24 13:55:56 -0300

    feat(admin): add new models

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Employees, Shifts, ShiftsEmployees
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Employees)
+admin.site.register(Shifts)
+admin.site.register(ShiftsEmployees)
