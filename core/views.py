f10fa0c3f577f8a8ebcdcae99581c05a805720d0
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-02-24 14:47:31 -0300

    feat(views): create a view to use custom signal

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Employees
+from .signals import event_signal
+from django.http import HttpResponse
 
 # Create your views here.
+def test_logging(request):
+    # Empty string
+    message = ''
+    try:
+        # Create an object
+        emp_inst = Employees.objects.create(first_name='user1',
+                                            last_name='last_user',
+                                            address='address',
+                                            age=93)
+        message += 'Employees object created: {} {}'.format(emp_inst.first_name, emp_inst.last_name)
+    except (Employees.DoesNotExist, ValueError) as e:
+        print('Error during employees creating occured: ', e)
+        # Convert ValueError error to string for successful concatenation
+        message += str(e)
+        emp_inst = None
+        pass
+
+    event_signal.send(sender='View: test_logging', message=message, instance=emp_inst)
+    return HttpResponse('bla')
